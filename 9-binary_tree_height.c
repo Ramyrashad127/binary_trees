f1d7ae326ed5b5c055279c760eb944182b830e6a
Ramyrashad127 <ramy41599@gmail.com>
2024-01-04 11:31:00 -0800

    new

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,14 +6,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t a = 0;
+	size_t b = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t a = 0;
-	size_t b = 0;
-
 	if (tree->right)
 	{
 		a = 1 + binary_tree_height(tree->right);
