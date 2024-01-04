d0fc17ff50fe60586a83982320c06c924d94c661
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2024-01-04 10:56:55 -0800

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	size_t a = binary_tree_height(tree->right), b = binary_tree_height(tree->left);
+	size_t a, b;
+
+	if (tree->right)
+		a = binary_tree_height(tree->right) + 1;
+	else
+		a = 0;
+	if (tree->left)
+		b = binary_tree_height(tree->left) + 1;
+	else
+		b = 0;
+
 	if (a > b)
 		return (a);
 	else
