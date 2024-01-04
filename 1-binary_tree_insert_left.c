a6357c7c6386b35f45ed8b02a319955eeda739d5
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2024-01-04 09:56:24 -0800

    task1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - insert left
+ * @parent: pointer
+ * @value: int
+ * Return: node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (!parent)
+		return (NULL);
+	binary_tree_t *new;
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	if (!(parent->left))
+	{
+		parent->left = new;
+	}
+	else
+	{
+		new->left = parent->left;
+		parent->left = new;
+	}
+	return (new);
+}
